fd7d6541775d8d90ab7e40db1c1b37b85fa1a471
Lilang Wang <lucaswang750@berkeley.edu>
2023-10-04 16:24:46 -0700

    Added function to process data into a json list
    Signed-off-by: Lilang Wang <lucaswang750@berkeley.edu>

--- demo/docbot/ingestion.py
+++ demo/docbot/ingestion.py
@@ -1,4 +1,5 @@
 from util import opensearch_connection_builder
+import os, json
 
 # Using a connection from the OpenSearch connection builder
 # to see an example of using the opensearch connection builder
@@ -8,7 +9,20 @@ from util import opensearch_connection_builder
 # check out the _bulk python client helper.
 
 # Documents should be ingested from the /data/ dir
+DEMOS_PATH = os.path.abspath(os.path.join(__file__,  "..", "..",".."))
+DOCUMENTATION_INDEX_PATH = "data/documentation-index.json"
+WEBSITE_INDEX_PATH = "data/website-index.json"
+
+def read_files_from_data() -> (list, list):
+  documentation_index = os.path.join(DEMOS_PATH, DOCUMENTATION_INDEX_PATH)
+  website_index = os.path.join(DEMOS_PATH, WEBSITE_INDEX_PATH)
+  with open(documentation_index) as f:
+    documentation_index_data = json.load(f)
+  with open(website_index) as f:
+    website_index_data = json.load(f)
+
+  return documentation_index_data, website_index_data
 
 if __name__=="__main__":
   # do ingestion
-  pass
+  documentation_index_data, website_index_data = read_files_from_data()
