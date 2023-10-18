0554aff77e88cedab45434d1d12911aa8182df37
Lucas Wang <lucaswang750@gmail.com>
2023-10-18 11:52:19 -0700

    discord send/receive messages
    Signed-off-by: Lucas Wang <lucaswang750@gmail.com>

--- demo/docbot/integration.py
+++ demo/docbot/integration.py
@@ -4,3 +4,29 @@
 
 # Here we will create classes for connecting into services like Discord
 # We should use the discord python client here
+import discord, sys
+import dotenv
+from os import getenv
+dotenv.load_dotenv()
+DISCORD_TOKEN= getenv("DISCORD_TOKEN")
+
+
+sys.path.append('./demo/')
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Successfully logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!docbot'):
+        msg = message.content
+        await message.channel.send('This is what you sent: ' + msg)
+client.run(DISCORD_TOKEN)
