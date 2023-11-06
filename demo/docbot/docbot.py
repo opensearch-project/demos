92a5e8ff08f4135f32539e76987293f352694ed5
RobertKilkenny <robert.kilkenny7@gmail.com>
2023-11-06 15:31:14 -0500

    Added check for if it exists in OpenSearch server.
    Signed-off-by: RobertKilkenny <robert.kilkenny7@gmail.com>

--- demo/docbot/docbot.py
+++ demo/docbot/docbot.py
@@ -1,12 +1,17 @@
 import sys
+from tkinter.filedialog import Open
+from token import OP
+from unittest import result
 from opensearchpy import OpenSearch
 sys.path.append('./demo/')
 
 from docbot.language_pipeline import generate_response
+from opensearchpy import OpenSearch
 
 class DocBot():
-  def __init__ (self):
+  def __init__ (self, open_search:OpenSearch):
       self.conversations = {}
+      self.OpenSearch = open_search
 
   def handle_message(self, message_object):
     # get conversation id form _conversation handler
@@ -21,9 +26,22 @@ class DocBot():
       # check if conversation exists. If it exists track conversation id in self.conversations by name: id
       id = self.conversations[conversation_name]
     else:
-      # if conversation doesn't exists create and then track using the above method
-      id = len(self.conversations) + 1
-      self.conversations[conversation_name] = id
+      # Check if it exists in OpenSearch
+      search_query = {
+        "query": {
+            "match": {
+                "name": conversation_name
+                }
+              }
+            }
+      result = self.OpenSearch.search(index='cohere', body=search_query)
+      if result['hits']['total']['value'] > 0:
+        data = result.json()
+        hits = data.get('hits', {}).get('hits', [])
+        id = hits[0]['id']
+      else:
+        id = len(self.conversations) + 1
+        self.conversations[conversation_name] = id
 
     #Then return the relevant ID
     if id == None:
